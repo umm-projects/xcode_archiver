2b120c225160088725f801e07694bd4605226cca
Tetsuya Mori <monry84@gmail.com>
2017-09-01 11:36:33 +0900

    :new: Add EnvironmentSetting
    * 環境設定として「Xcode Archive を行うかどうか？」のフィールドを追加します

---
+++ Assets/Scripts/UnityModule/Settings/EnvironmentSetting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityModule.Settings {
+
+    /// <summary>
+    /// 環境設定を拡張
+    /// </summary>
+    // ReSharper disable once PartialTypeWithSinglePart
+    public partial class EnvironmentSetting : Setting<EnvironmentSetting> {
+
+        /// <summary>
+        /// Xcode Archive を起動するかどうかの実体
+        /// </summary>
+        [SerializeField]
+        private bool shouldRunXcodeArchive;
+
+        /// <summary>
+        /// Xcode Archive を起動するかどうか
+        /// </summary>
+        public bool ShouldRunXcodeArchive {
+            get {
+                return this.shouldRunXcodeArchive;
+            }
+            set {
+                this.shouldRunXcodeArchive = value;
+            }
+        }
+
+    }
+
+}
