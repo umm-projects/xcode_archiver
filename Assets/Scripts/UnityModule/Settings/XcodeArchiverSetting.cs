265640819ca757e65d4bbb41e21498621d0f7cc7
Tetsuya Mori <monry84@gmail.com>
2018-05-31 20:48:52 +0900

    :sparkles: Remove partial classes

---
+++ Assets/Scripts/UnityModule/Settings/XcodeArchiverSetting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace UnityModule.Settings {
+
+    public class XcodeArchiverSetting : Setting<XcodeArchiverSetting>
+    {
+        [SerializeField]
+        private bool shouldRunXcodeArchive = true;
+
+        public bool ShouldRunXcodeArchive => shouldRunXcodeArchive;
+
+        [SerializeField]
+        private bool shouldExportAppStoreArchive = true;
+
+        public bool ShouldExportAppStoreArchive => shouldExportAppStoreArchive;
+
+#if UNITY_EDITOR
+        [UnityEditor.MenuItem("Assets/Create/Settings/Xcode Archiver Setting")]
+        public static void CreateSettingAsset()
+        {
+            CreateAsset(true);
+        }
+#endif
+    }
+
+}
